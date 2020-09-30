96752b186e71ba288cf424aea1bbdfe0ec0f94df
joeruiz <joseph.ruiz@quinnipiac.edu>
2020-09-30 18:25:02 -0400

    add bootstrap button

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Joe Ruiz Web Developer Portfolio</h1>
-        <>
-          <Button variant="outline-info">Info</Button>
-        </>
+        <Button variant="outline-info">View Portfolio</Button>
       </header>
     </div>
   );
