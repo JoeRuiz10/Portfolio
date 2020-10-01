a3927afc960df645208111b9b4129dc6809e3187
joeruiz <joseph.ruiz@quinnipiac.edu>
2020-10-01 19:37:31 -0400

    app.js into component update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import logo from "./images/logo.png";
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 id="title">Joe Ruiz Web Developer Portfolio</h1>
-        <Button variant="outline-info">View Portfolio</Button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 id="title">Joe Ruiz Web Developer Portfolio</h1>
+          <Button variant="outline-info">View Portfolio</Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
