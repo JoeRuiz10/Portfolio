740407bad21200bc4f6e8f4cacd8ff53b7092e17
joeruiz <joseph.ruiz@quinnipiac.edu>
2020-10-05 18:07:52 -0400

    Relaunch

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 import React, { Component } from "react";
 
-class App extends Component {
+class App {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 id="title">Joe Ruiz Web Developer Portfolio</h1>
-          <Button variant="outline-info">View Portfolio</Button>
-        </header>
-      </div>
-    );
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 id="title">Joe Ruiz Web Developer Portfolio</h1>
+        <Button variant="outline-info">View Portfolio</Button>
+      </header>
+    </div>;
   }
 }
 
