13856c2cf92a181565b777c04d234f7d896046cc
joeruiz <joseph.ruiz@quinnipiac.edu>
2020-10-25 23:55:03 -0400

    1.26 updated dynamics and hover on social media links

--- src/Components/socials.jsx
+++ src/Components/socials.jsx
@@ -11,22 +11,22 @@ class Socials extends Component {
     return (
       <div className="row container-fluid justify-content-center">
         <span>
-          <a className="fab fa-github" href="https://github.com/Jruiz10"  style={{ textDecoration: 'none' }} ></a>
+          <a id="github" className="fab fa-github" href="https://github.com/Jruiz10"  style={{ textDecoration: 'none' }} ></a>
         </span>
         <span>
-        <a className="fab fa-linkedin-in" href="https://www.linkedin.com/in/josephsruiz/" style={{ textDecoration: 'none' }} ></a>
+        <a id="linkedin" className="fab fa-linkedin-in" href="https://www.linkedin.com/in/josephsruiz/" style={{ textDecoration: 'none' }} ></a>
         </span>
         <span>
-        <a className="fab fa-youtube" href="https://www.youtube.com/channel/UC2jtCZU0oRqI9dUM4H_oZJA?view_as=subscriber" style={{ textDecoration: 'none' }}></a>
+        <a id="youtube" className="fab fa-youtube" href="https://www.youtube.com/channel/UC2jtCZU0oRqI9dUM4H_oZJA?view_as=subscriber" style={{ textDecoration: 'none' }}></a>
         </span>
         <span>
-        <a className="fab fa-instagram" href="https://www.instagram.com/acupof.joe/" style={{ textDecoration: 'none' }}></a>
+        <a id="instagram" className="fab fa-instagram" href="https://www.instagram.com/acupof.joe/" style={{ textDecoration: 'none' }}></a>
         </span>
         <span>
-        <a className="fab fa-facebook-f" href="https://www.facebook.com/profile.php?id=100004866738900" style={{ textDecoration: 'none' }}></a>
+        <a id="facebook" className="fab fa-facebook-f" href="https://www.facebook.com/profile.php?id=100004866738900" style={{ textDecoration: 'none' }}></a>
         </span>
         <span>
-        <a className="fab fa-twitter" href="https://twitter.com/acupof__joe" style={{ textDecoration: 'none' }}></a>
+        <a id="twitter" className="fab fa-twitter" href="https://twitter.com/acupof__joe" style={{ textDecoration: 'none' }}></a>
         </span>
       </div>
     );
