092b4e60db1dbd487d7d89ac0d0605f88c1e8d31
Jruiz10 <46909779+Jruiz10@users.noreply.github.com>
2021-01-29 19:17:18 -0500

    trying line under titles
    test 1

---
+++ src/creator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class CreatorPage extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CreatorPage;
